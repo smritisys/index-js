1ea52244eaf283dfe7949d4dd92de054ab3ca1cf
smritisys <sohanasmriti44@gmail.com>
2024-06-03 20:26:01 +0600

    mark sheet has completed

---
+++ index.js
@@ -0,0 +1,213 @@
+// let a = 25;
+// console.log(typeof(a));
+
+// let b = "25"
+// console.log(typeof(b));
+
+// let a = 25 + 6 + " Shuvo"
+// console.log(a);
+// let myName = "Riyaz "
+// let b = "Shuvo " + 25 + 6;
+// console.log(b);
+
+
+// var fatchData = 45;
+// console.log(a);
+// var a = 55;
+// console.log(a);
+
+// let a = 45
+// console.log(a);
+
+//  let a = 55;
+// console.log(a);
+
+
+
+// var a = 45
+// let b = 55;
+// const c = 65;
+
+
+// {
+//     var a = 45
+//     let b = 55;
+//     const c = 65
+// }
+// // console.log(b);
+// // console.log(c);
+// console.log(a);
+
+
+// function sum() {
+//     let a = 34;
+//     const b = 45
+//     var c = 21;
+// }
+
+// let a = 7;
+
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+
+// let a = 7;
+
+// a = a + 8;
+// a+=8;
+
+// 1 - positive or negative
+// let age = 18;
+
+// if (age >= 18) {
+//     console.log("Adult");
+// }
+// else {
+//     console.log("Child ");
+// }
+
+
+// 2- odd or even
+// let  = 700;
+
+// if (-600) {
+//     console.log("Positive");
+// }
+// else {
+//     console.log("Nagative ");
+// }
+
+
+// 1- positive or neg
+
+// let number = 0;
+
+// console.log(number>0);
+
+// if (number > 0) {
+//     console.log("Positive");
+// }
+// else if (number == 0) {
+//     console.log("Zero");
+// }
+// else {
+//     console.log("Negative");
+// }
+
+// || &&
+
+// 2- even or odd
+// let number = 17;
+
+// if (number % 2!= 0) {
+//     console.log("Odd number");
+// }
+// else {
+//     console.log("Even Number");
+// }
+
+// 3 - Find max between 2 numbers
+// let number1 = 11;
+// let number2 = 11;
+
+// if (number1 > number2) {
+//     console.log("Number1 is grater number");
+// }
+// else if (number1 == number2) {
+//     console.log("Both number is equal");
+// }
+// else {
+//     console.log("Number2 is grater number");
+// }
+
+//  4- Find max among 3 numbers
+
+// let number1 = 11
+// let number2 = 13
+// let number3 = 15
+
+
+// if ((number1 > number2) && (number1 > number3)) {
+//     console.log("Number1 is grater number");
+// }
+// else if (number2 > number1 && number2 > number3) {
+//     console.log("Number2 is grater number");
+// }
+// else {
+//     console.log("Number3 is grater number");
+// }
+
+// 5- Check vowel or consonant
+
+// let Char = 'A';
+// if (Char === 'a' || Char === 'e' || Char === 'i' || Char === 'o' || Char === 'u'||Char=='A') {
+//     console.log("Vowel");
+// }
+// else {
+//     console.log("consonnat");
+// }
+
+// 6- input week number and console total day.
+
+// let week = 5;
+
+// console.log(week * 7);
+
+// 7- input month number and console number of days in that month.
+
+// let month = 1;
+// let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+// // year %4==0 && year %100!=0 || year%400==0
+
+// if (month == 1) {
+//     console.log("31 days");
+// }
+// else if (month == 2) {
+//     console.log("28 days");
+// }
+
+// 8- Number of day in a date
+// 31-12-1800
+
+// let year = 2004
+// let month = 2
+// let day = 1
+// let totalDays = 0;
+
+
+// if (month == 1) {
+//     totalDays = day
+// }
+// else if (month == 2) {
+//     totalDays = 31 + day
+// }
+// else if (month == 3) {
+//     totalDays = 31 + 28 + day
+// }
+
+
+// let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+// let month = 3;
+// let year = 2008
+// let day = 1
+
+let marks = 0
+
+if (marks <= 100 && marks >= 0) {
+    if (marks >= 80) {
+        console.log("A+");
+    }
+    else if (marks >= 70&& marks<=79) {
+        console.log("A");
+    }
+    else if (marks <= 32) {
+        console.log("Fail");
+    }
+}
+else {
+    console.log("Invalid marks");
+}
+
+
+
