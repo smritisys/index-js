8715f5fbd45a3eaef93602099da9699676f7b4fe
smritisys <sohanasmriti44@gmail.com>
2024-06-10 12:09:20 +0600

    changes

--- index.js
+++ index.js
@@ -1,214 +1,454 @@
-// let a = 25;
-// console.log(typeof(a));
-
-// let b = "25"
-// console.log(typeof(b));
-
-// let a = 25 + 6 + " Shuvo"
-// console.log(a);
-// let myName = "Riyaz "
-// let b = "Shuvo " + 25 + 6;
-// console.log(b);
+// // let a = 25;
+// // console.log(typeof(a));
 
 
-// var fatchData = 45;
-// console.log(a);
-// var a = 55;
-// console.log(a);
+// // let b = "25"
+// // console.log(typeof(b));
 
-// let a = 45
-// console.log(a);
-
-//  let a = 55;
-// console.log(a);
-
-
-
-// var a = 45
-// let b = 55;
-// const c = 65;
-
-
-// {
-//     var a = 45
-//     let b = 55;
-//     const c = 65
-// }
+// // let a = 25 + 6 + " Shuvo"
+// // console.log(a);
+// // let myName = "Riyaz "
+// // let b = "Shuvo " + 25 + 6;
 // // console.log(b);
-// // console.log(c);
-// console.log(a);
 
 
-// function sum() {
-//     let a = 34;
-//     const b = 45
-//     var c = 21;
-// }
+// // var fatchData = 45;
+// // console.log(a);
+// // var a = 55;
+// // console.log(a);
 
-// let a = 7;
+// // let a = 45
+// // console.log(a);
 
-// console.log(a++);
-// console.log(a);
-// console.log(++a);
+// //  let a = 55;
+// // console.log(a);
 
-// let a = 7;
 
-// a = a + 8;
-// a+=8;
 
-// 1 - positive or negative
-// let age = 18;
+// // var a = 45
+// // let b = 55;
+// // const c = 65;
 
-// if (age >= 18) {
-//     console.log("Adult");
+
+// // {
+// //     var a = 45
+// //     let b = 55;
+// //     const c = 65
+// // }
+// // // console.log(b);
+// // // console.log(c);
+// // console.log(a);
+
+
+// // function sum() {
+// //     let a = 34;
+// //     const b = 45
+// //     var c = 21;
+// // }
+
+// // let a = 7;
+
+// // console.log(a++);
+// // console.log(a);
+// // console.log(++a);
+
+// // let a = 7;
+
+// // a = a + 8;
+// // a+=8;
+
+// // 1 - positive or negative
+// // let age = 18;
+
+// // if (age >= 18) {
+// //     console.log("Adult");
+// // }
+// // else {
+// //     console.log("Child ");
+// // }
+
+
+// // 2- odd or even
+// // let  = 700;
+
+// // if (-600) {
+// //     console.log("Positive");
+// // }
+// // else {
+// //     console.log("Nagative ");
+// // }
+
+
+// // 1- positive or neg
+
+// // let number = 0;
+
+// // console.log(number>0);
+
+// // if (number > 0) {
+// //     console.log("Positive");
+// // }
+// // else if (number == 0) {
+// //     console.log("Zero");
+// // }
+// // else {
+// //     console.log("Negative");
+// // }
+
+// // || &&
+
+// // 2- even or odd
+// // let number = 17;
+
+// // if (number % 2!= 0) {
+// //     console.log("Odd number");
+// // }
+// // else {
+// //     console.log("Even Number");
+// // }
+
+// // 3 - Find max between 2 numbers
+// // let number1 = 11;
+// // let number2 = 11;
+
+// // if (number1 > number2) {
+// //     console.log("Number1 is grater number");
+// // }
+// // else if (number1 == number2) {
+// //     console.log("Both number is equal");
+// // }
+// // else {
+// //     console.log("Number2 is grater number");
+// // }
+
+// //  4- Find max among 3 numbers
+
+// // let number1 = 11
+// // let number2 = 13
+// // let number3 = 15
+
+
+// // if ((number1 > number2) && (number1 > number3)) {
+// //     console.log("Number1 is grater number");
+// // }
+// // else if (number2 > number1 && number2 > number3) {
+// //     console.log("Number2 is grater number");
+// // }
+// // else {
+// //     console.log("Number3 is grater number");
+// // }
+
+// // 5- Check vowel or consonant
+
+// // let Char = 'A';
+// // if (Char === 'a' || Char === 'e' || Char === 'i' || Char === 'o' || Char === 'u'||Char=='A') {
+// //     console.log("Vowel");
+// // }
+// // else {
+// //     console.log("consonnat");
+// // }
+
+// // 6- input week number and console total day.
+
+// // let week = 5;
+
+// // console.log(week * 7);
+
+// // 7- input month number and console number of days in that month.
+
+// // let month = 1;
+// // let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+// // // year %4==0 && year %100!=0 || year%400==0
+
+// // if (month == 1) {
+// //     console.log("31 days");
+// // }
+// // else if (month == 2) {
+// //     console.log("28 days");
+// // }
+
+// // 8- Number of day in a date
+// // 31-12-1800
+
+// // let year = 2004
+// // let month = 2
+// // let day = 1
+// // let totalDays = 0;
+
+
+// // if (month == 1) {
+// //     totalDays = day
+// // }
+// // else if (month == 2) {
+// //     totalDays = 31 + day
+// // }
+// // else if (month == 3) {
+// //     totalDays = 31 + 28 + day
+// // }
+
+
+// // let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+// // let month = 3;
+// // let year = 2008
+// // let day = 1
+
+// let marks = 0
+
+// if (marks <= 100 && marks >= 0) {
+//     if (marks >= 80) {
+//         console.log("A+");
+//     }
+//     else if (marks >= 70&& marks<=79) {
+//         console.log("A");
+//     }
+//     else if (marks <= 32) {
+//         console.log("Fail");
+//     }
 // }
 // else {
-//     console.log("Child ");
+//     console.log("Invalid marks");
+// }
+// 10- calculate electricity bill from given condition
+//
+// let unit = 100
+// if (unit <=50){
+//        amount = unit *.50;
+// }
+// else if (unit <=100){
+//     amount = ((50*.50)+(unit-50)*.75);
+// }
+// else if (unit <= 150){
+//     amount = ((50*.50)+(100-50)*.75)+(unit - 100 )*1.20;
+// }
+// else {// above 250 units
+
+//   ("  amount = (50*.50) +((100-50)*.75)+((200-100)*1.20)+(unit-250) * 1.50");
+
 // }
 
+// console.log(amount);
 
-// 2- odd or even
-// let  = 700;
 
-// if (-600) {
-//     console.log("Positive");
+// 10- calculate electricity bill from given condition
+
+// let unit = 252;
+
+// let amount = 0;
+
+// if (unit <= 50) {
+//   amount = unit * 0.50;
 // }
-// else {
-//     console.log("Nagative ");
+// else if (unit <= 150) {
+//   amount = 25 + (unit - 50) * .75
 // }
-
-
-// 1- positive or neg
-
-// let number = 0;
-
-// console.log(number>0);
-
-// if (number > 0) {
-//     console.log("Positive");
+// else if (unit <= 250) {
+//   amount = 100 + (unit - 150) * 1.20
 // }
-// else if (number == 0) {
-//     console.log("Zero");
+// else if (unit > 250) {
+//   amount = 220 + (unit - 250) * 1.5;
 // }
-// else {
-//     console.log("Negative");
-// }
+// console.log(amount + 55);
 
-// || &&
+// console.log("Shuvo");
+// console.log("Shuvo");
+// console.log("Shuvo");
+// console.log("Shuvo");
+// console.log("Shuvo");
 
-// 2- even or odd
-// let number = 17;
+// forf(init;condition;increment/decrement)
 
-// if (number % 2!= 0) {
-//     console.log("Odd number");
-// }
-// else {
-//     console.log("Even Number");
+// for (let i = 1; i <= 15; i++) {
+//   console.log(i, ". Shuvo");
 // }
 
-// 3 - Find max between 2 numbers
-// let number1 = 11;
-// let number2 = 11;
-
-// if (number1 > number2) {
-//     console.log("Number1 is grater number");
-// }
-// else if (number1 == number2) {
-//     console.log("Both number is equal");
-// }
-// else {
-//     console.log("Number2 is grater number");
+// for (i = 1; i <= 100; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
 // }
 
-//  4- Find max among 3 numbers
+// while(conditon){}
+// let i = 100
 
-// let number1 = 11
-// let number2 = 13
-// let number3 = 15
-
-
-// if ((number1 > number2) && (number1 > number3)) {
-//     console.log("Number1 is grater number");
-// }
-// else if (number2 > number1 && number2 > number3) {
-//     console.log("Number2 is grater number");
-// }
-// else {
-//     console.log("Number3 is grater number");
+// while (i >= 1) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+//   i--
 // }
 
-// 5- Check vowel or consonant
+// do{} while(condition)
+// let i = 5;
+// do {
+//   console.log(i);
+// } while (i > 5);
 
-// let Char = 'A';
-// if (Char === 'a' || Char === 'e' || Char === 'i' || Char === 'o' || Char === 'u'||Char=='A') {
-//     console.log("Vowel");
+// let a = 5.49
+
+// console.log(Math.PI);
+// console.log(Math.round(a));
+// console.log(Math.ceil(a));
+
+// console.log(Math.sqrt(9));
+
+// let a = 9;
+// let b = 6;
+// 5 + 6 = 11
+// console.log(a, " + ", b, " = ", a + b);
+// console.log(a + " + " + b + " = " + Number(a + b));
+
+// 5 X 1 = 5
+// let a = 5
+// let b = 6
+// for (let i = 1; i <= 10; i++) {
+//   // console.log(a, " X ", i, " = ", i * a);
+//   console.log(`${a} X ${i} = ${i*a}`);
 // }
-// else {
-//     console.log("consonnat");
+
+// let name = "Shuvo"
+
+// let lorem = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+// Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+// when an unknown printer took a galley of type and scrambled it is Mr. ${name}
+// to make a type specimen book. It has survived not only five centuries, but also
+// the leap into electronic typesetting, remaining essentially unchanged. It was
+// popularised in the 1960s with the release of Letraset sheets containing
+// Lorem Ipsum passages, and more recently with desktop publishing software
+// like Aldus PageMaker including versions of Lorem Ipsum.`
+
+// console.log(lorem);
+
+// int [] arra = [];
+
+// let arr = ["w45", 34, true]
+
+// console.log(arr[2]);
+
+// let numbers = [1, 5, 3, 4]
+// let sum = 0;
+
+// for (let i = 0; i < 4; i++) {
+//   sum =  sum + numbers[i]
 // }
+// console.log(sum);
 
-// 6- input week number and console total day.
-
-// let week = 5;
-
-// console.log(week * 7);
-
-// 7- input month number and console number of days in that month.
-
-// let month = 1;
+// let year = 2001
 // let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-// // year %4==0 && year %100!=0 || year%400==0
-
-// if (month == 1) {
-//     console.log("31 days");
-// }
-// else if (month == 2) {
-//     console.log("28 days");
-// }
-
-// 8- Number of day in a date
-// 31-12-1800
-
-// let year = 2004
-// let month = 2
-// let day = 1
+// let day = 1;
+// let month = 2;
 // let totalDays = 0;
 
+// for (let i = 0; i < month; i++) {
+//   if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+//     months[1] = 29;
+//   }
+//   totalDays = months[i] + totalDays;
 
-// if (month == 1) {
-//     totalDays = day
 // }
-// else if (month == 2) {
-//     totalDays = 31 + day
+// console.log(totalDays + day);
+
+// for (let i = 0; i < month; i++) {
+//   // if (i == 1 && (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+//   //   months[1] = 29
+//   // }
+//   // totalDays = months[i] + totalDays
+
+
 // }
-// else if (month == 3) {
-//     totalDays = 31 + 28 + day
+// console.log(totalDays + day);
+
+// let arr = [11, 143, 14, 16]
+
+// arr.push(324)
+
+// console.log(arr);
+
+
+// for in 
+
+// let numbers = [11, 13, 15, 17]
+
+
+// for (let i in numbers) {
+// console.log(numbers[i]  , i );
 // }
 
+// for (const a of numbers) {
+//   console.log(a);
+// }
 
-// let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+// let arr = [11, 12, 13,14]
+// let sum = 0;
 
-// let month = 3;
-// let year = 2008
-// let day = 1
+// for (let i = 0; i <=4; i++) {
+//   sum = arr[i]+sum;
+// }
+// console.log(sum);
+// without argument without return statement
+// function sum() {
+//   let a = 88
+//   let b = 11
+//   console.log(a + b);
+// }
 
-let marks = 0
+// sum()
 
-if (marks <= 100 && marks >= 0) {
-    if (marks >= 80) {
-        console.log("A+");
-    }
-    else if (marks >= 70&& marks<=79) {
-        console.log("A");
-    }
-    else if (marks <= 32) {
-        console.log("Fail");
-    }
+// without argument with return statement
+// function sub() {
+//   let a = 34;
+//   let b = 45;
+//   return b - a;
+// }
+
+// console.log(sub());
+
+
+// with argument and without return statement
+
+
+// function sum(a, b  ,c ) {
+//   console.log(a + b-c);
+// }
+
+// sum(11, 14 , 34)
+
+// with argument and with return statement
+
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// console.log(sum(2,45,6));
+
+function sum() {
+  let a = 900
+  let b = 9
+  console.log(a * b);
 }
-else {
-    console.log("Invalid marks");
+
+sum()
+
+
+// 02
+function sub() {
+  let a = 60;
+  let b = 2;
+  return (a % b);
 }
 
+console.log(sub());
 
+// 03  
+function sum(a, b, c) {
+  console.log(a * b * c);
+}
 
+sum(11, 22, 33)
 
+// 04
+function sum(a, b, c) {
+  return a % b % c;
+}
+
+console.log(sum(2, 3, 6));
